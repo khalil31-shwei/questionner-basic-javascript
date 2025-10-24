1b30b9b6bd399e8bf58eae036a8510fb2c14175b
unknown <khalilbad709@gmail.com>
2025-10-24 23:49:18 +0100

    questionner

---
+++ equation.js
@@ -0,0 +1,20 @@
+var x = 2 
+var a = prompt ( " wchhal katsawi a ")
+var b = prompt ( " wchhal katsawi of b ")
+var c = prompt ( " wchhal katsawi of c ")
+ a = Number (a);
+  b = Number (b);
+   c = Number (c);
+
+   var result = a*x*x+b*x+c;
+
+   if( result == 0)
+   { alert ( "bravo 3lik");}
+  else { alert ( "3awd 7seeb achrif");}
+
+   
+ 
+
+
+   
+
